885224c24e8cf8db727c675d39ae1406c948f166
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-14 08:47:14 -0800

    Update navbar.tsx

--- components/geistdocs/navbar.tsx
+++ components/geistdocs/navbar.tsx
@@ -20,27 +20,29 @@ type NavbarProps = {
 };
 
 export const Navbar = ({ children, items, suggestions }: NavbarProps) => (
-  <header className="sticky top-0 z-40 flex h-16 w-full items-center gap-6 border-b bg-sidebar px-4 py-3.5 md:px-6">
-    <div className="flex shrink-0 items-center gap-2.5">
-      <a href="https://vercel.com/" rel="noopener" target="_blank">
-        <SiVercel className="size-5" />
-      </a>
-      <SlashIcon className="size-5 text-border" />
-      <Link href="/">{children}</Link>
-    </div>
-    <div className="hidden flex-1 md:block">
-      <DesktopMenu items={items} />
-    </div>
-    <div className="hidden flex-1 shrink-0 items-center justify-end gap-1 md:flex">
-      <RSSButton />
-      <GitHubButton />
-      <ThemeToggle />
-      <SearchButton />
-      <Chat suggestions={suggestions} />
-    </div>
-    <div className="ml-auto flex items-center gap-1 md:hidden">
-      <SearchButton />
-      <MobileMenu />
+  <header className="sticky top-0 z-40 h-16 w-full gap-6 border-b bg-sidebar">
+    <div className="mx-auto flex w-(--fd-layout-width) items-center px-4 py-3.5 md:px-6">
+      <div className="flex shrink-0 items-center gap-2.5">
+        <a href="https://vercel.com/" rel="noopener" target="_blank">
+          <SiVercel className="size-5" />
+        </a>
+        <SlashIcon className="size-5 text-border" />
+        <Link href="/">{children}</Link>
+      </div>
+      <div className="hidden flex-1 md:block">
+        <DesktopMenu items={items} />
+      </div>
+      <div className="hidden flex-1 shrink-0 items-center justify-end gap-1 md:flex">
+        <RSSButton />
+        <GitHubButton />
+        <ThemeToggle />
+        <SearchButton />
+        <Chat suggestions={suggestions} />
+      </div>
+      <div className="ml-auto flex items-center gap-1 md:hidden">
+        <SearchButton />
+        <MobileMenu />
+      </div>
     </div>
   </header>
 );
